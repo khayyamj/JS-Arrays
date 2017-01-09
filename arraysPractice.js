26da187c47a999fe7fbfa99dca0c386250860399
Khayyam Jones <Khayyam.Jones@gmail.com>
2017-01-09 12:29:54 -0700

    completed reverse and removeItem problems

--- arraysPractice.js
+++ arraysPractice.js
@@ -105,7 +105,15 @@ var getRandomArbitrary = function() {
 // Your job is to write a function named finder that will get a random number (by invoking getRandomArbitrary), then loop through the array (that will be passed in as a parameter) to see if that random number is in the array. If it is, return true, if it's not, return false
 
   //Code Here
-
+function finder(numberSet) {
+  var randomNum = getRandomArbitrary();
+  for (var i=0; i<numberSet.length; i++) {
+    if (randomNum === numberSet[i]) {
+      return true;
+    }
+  }
+  return false;
+}
   //Code Here
 
 
@@ -117,7 +125,14 @@ var str = 'this is my sentence';
 //Write a function called reverse that takes a given str as it's only argument and returns that string after it's been reversed
 
   //Code Here
-
+  function reverse(str) {
+      var reverseStr = "";
+      for (var i=str.length - 1; i>=0; i--) {
+        var letter = str.charAt(i);
+        reverseStr = reverseStr.concat(letter);
+      }
+    return reverseStr;
+  }
 
 //Next Problem
 
